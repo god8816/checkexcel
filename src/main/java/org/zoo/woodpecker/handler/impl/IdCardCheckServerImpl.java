a9665d5d57f84ce883d7cb4328a3a90e27fef99a
god8816 <948351520@qq.com>
2021-11-01 18:23:17 +0800

    no message

--- src/main/java/org/zoo/woodpecker/handler/impl/IdCardCheckServerImpl.java
+++ src/main/java/org/zoo/woodpecker/handler/impl/IdCardCheckServerImpl.java
@@ -1,7 +1,9 @@
 package org.zoo.woodpecker.handler.impl;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import org.zoo.woodpecker.annotation.EmptyCheckType;
 import org.zoo.woodpecker.handler.ExcelCheckServer;
 import org.zoo.woodpecker.util.ClassUtils.FieldCache;
 import org.zoo.woodpecker.util.StringUtil;
@@ -14,15 +16,58 @@ import org.zoo.woodpecker.util.StringUtil;
 public class IdCardCheckServerImpl extends ExcelCheckServer{
 
 	public Boolean doCheck(FieldCache fieldCache) {
-		String phone = StringUtil.toString(fieldCache.getValue());
-		if(StringUtil.isEmpty(phone)) {
-			return false;
+		String value = StringUtil.toString(fieldCache.getValue());
+		String regularExpVlue = fieldCache.getRegularExp();
+		if(StringUtil.isEmpty(regularExpVlue)) {
+			regularExpVlue = "^(\\d{18,18}|\\d{15,15}|\\d{17,17}X)$";
 		}
-		Pattern p = Pattern.compile("^(\\d{18,18}|\\d{15,15}|\\d{17,17}X)$");
-		Matcher m = p.matcher(phone); 
+		
+		if(StringUtil.isNotEmpty(value)) {
+			return regularExpCheck(regularExpVlue,value) && containParamCheck(fieldCache) && notContainParam(fieldCache);
+		}else {
+			if(EmptyCheckType.empty.name().equals(fieldCache.getCheckEmptyType().name())) {
+				return true;
+			}else {
+				return false;
+			}
+		}
+	}
+	
+	/**
+	 * 正则表达式校验
+	 * */
+	public Boolean regularExpCheck(String regularExpVlue,String value) {
+		Pattern p = Pattern.compile(regularExpVlue);
+		Matcher m = p.matcher(value); 
 		Boolean status =  m.matches();
 		return status;
 	}
+	
+	
+	/**
+	 * 包含判断
+	 * */
+	public Boolean containParamCheck(FieldCache fieldCache) {
+		String[] arr = fieldCache.getContainParam();
+		String value = StringUtil.toString(fieldCache.getValue());
+		
+		if(Objects.nonNull(arr) && arr.length>0) {
+			return Arrays.asList(arr).contains(value);
+		}
+		return true;
+	}
 
+	/**
+	 * 不包含判断
+	 * */
+	public Boolean notContainParam(FieldCache fieldCache) {
+		String[] arr = fieldCache.getContainParam();
+		String value = StringUtil.toString(fieldCache.getValue());
+		
+		if(Objects.nonNull(arr) && arr.length>0) {
+			return !Arrays.asList(arr).contains(value);
+		}
+		return true;
+	}
 	 
 }
--- src/main/java/org/zoo/woodpecker/handler/impl/PhoneCheckServerImpl.java
+++ src/main/java/org/zoo/woodpecker/handler/impl/PhoneCheckServerImpl.java
@@ -1,7 +1,8 @@
 package org.zoo.woodpecker.handler.impl;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.zoo.woodpecker.annotation.EmptyCheckType;
 import org.zoo.woodpecker.handler.ExcelCheckServer;
 import org.zoo.woodpecker.util.ClassUtils.FieldCache;
@@ -22,10 +23,7 @@ public class PhoneCheckServerImpl extends ExcelCheckServer{
 		}
 		
 		if(StringUtil.isNotEmpty(value)) {
-			Pattern p = Pattern.compile(regularExpVlue);
-			Matcher m = p.matcher(value); 
-			Boolean status =  m.matches();
-			return status;
+			return regularExpCheck(regularExpVlue,value) && containParamCheck(fieldCache) && notContainParam(fieldCache);
 		}else {
 			if(EmptyCheckType.empty.name().equals(fieldCache.getCheckEmptyType().name())) {
 				return true;
@@ -34,6 +32,42 @@ public class PhoneCheckServerImpl extends ExcelCheckServer{
 			}
 		}
 	}
+	
+	/**
+	 * 正则表达式校验
+	 * */
+	public Boolean regularExpCheck(String regularExpVlue,String value) {
+		Pattern p = Pattern.compile(regularExpVlue);
+		Matcher m = p.matcher(value); 
+		Boolean status =  m.matches();
+		return status;
+	}
+	
+	
+	/**
+	 * 包含判断
+	 * */
+	public Boolean containParamCheck(FieldCache fieldCache) {
+		String[] arr = fieldCache.getContainParam();
+		String value = StringUtil.toString(fieldCache.getValue());
+		
+		if(Objects.nonNull(arr) && arr.length>0) {
+			return Arrays.asList(arr).contains(value);
+		}
+		return true;
+	}
 
+	/**
+	 * 不包含判断
+	 * */
+	public Boolean notContainParam(FieldCache fieldCache) {
+		String[] arr = fieldCache.getContainParam();
+		String value = StringUtil.toString(fieldCache.getValue());
+		
+		if(Objects.nonNull(arr) && arr.length>0) {
+			return !Arrays.asList(arr).contains(value);
+		}
+		return true;
+	}
 	 
 }
